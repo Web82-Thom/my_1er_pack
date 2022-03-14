29d62d5870eda86969063ddf6b5835a13c47bbcd
unknown <thom.orta@gmail.com>
2022-03-14 16:52:31 +0100

    check version OS

--- lib/my_1er_pack.dart
+++ lib/my_1er_pack.dart
@@ -1,7 +1,10 @@
 library my_1er_pack;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dart:io';
+
+class Adaptive {
+  // static bool isIOS() => (Platform.isIOS);
+  static bool isIOS() => (!Platform.isIOS);
 }
